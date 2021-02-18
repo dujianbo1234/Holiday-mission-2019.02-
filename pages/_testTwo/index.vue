0350b94f4b5efe2ba3ca83cd20e958d4225c9864
dujianbo1234 <461954137@qq.com>
2021-02-18 21:12:54 +0800

    试一试

---
+++ pages/_testTwo/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-card>
+    <h1>{{title + ':' + $route.params.testTwo}}</h1>
+    <nuxt-link to="/">首页</nuxt-link>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '_testTwo/index.vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
